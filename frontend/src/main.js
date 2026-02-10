592e37bff54112c2bb85ee5cca03590029734daa
Tomoharu Ono <rio.carbon@gmail.com>
2026-02-10 18:31:53 +0100

    画面表示上の問題修正

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
 const vuetify = createVuetify()
